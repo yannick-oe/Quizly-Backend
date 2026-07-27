335b2690e098b6c7528ad77e6069127aa5a5c653
Yannick Oe <yannickoetelshoven@gmail.com>
2026-07-27 14:52:13 +0200

    chore: require authentication by default

--- core/settings.py
+++ core/settings.py
@@ -154,6 +154,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "auth_app.api.authentication.CookieJWTAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 
